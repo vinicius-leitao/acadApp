f45a829fe044ff9b86fbd45de5e099f9d2a3705d
Vinicius Santos <vinileitao2000@gmail.com>
2023-01-03 19:07:42 -0300

    fix: change professorContract functions

--- academic/src/components/Professor/index.js
+++ academic/src/components/Professor/index.js
@@ -2,43 +2,48 @@ import React from "react";
 import { Card } from "../Card";
 import { ethers } from "ethers";
 import { useEffect, useState } from 'react';
-
+import contractAddrs from '../../contractsAddrs.json'
 import '../Content.css';
 
 const ProfessorAbi = require("../../artifacts/contracts/ProfessorContract.sol/ProfessorContract.json").abi;
 
 const Professor = () => {
-  const ProfessorAddr = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
 
-  const [signer, setSigner] = useState();
-  const [signerAddress, setSignerAddress] = useState();
-  const [contract, setContract] = useState();
+  const {AcademicAddr, AlunoContractAddr, ProfessorContractAddr, DisciplinaContractAddr} = contractAddrs
+  const [signer, setSigner] = useState()
+  const [signerAddress, setSignerAdress] = useState()
+  const [professorContract, setProfessorContract] = useState()
+
 
   useEffect(() => {
+
     async function connect() {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
       await provider.send("eth_requestAccounts", []);
-      const signer2 = provider.getSigner();
-      setSigner(signer2);
-      setSignerAddress(await signer2.getAddress());
-      const professorContract = new ethers.Contract(
-        ProfessorAddr,
-        ProfessorAbi,
-        provider
-      );
-      setContract(professorContract.connect(signer2));
+      const currentSigner = provider.getSigner()
+      setSigner(currentSigner)
+      const signerAddress = await currentSigner.getAddress()
+      setSignerAdress(signerAddress);
+      const ProfessorContractInstance = new ethers.Contract(ProfessorContractAddr, ProfessorAbi, currentSigner)
+      setProfessorContract(ProfessorContractInstance);
     }
+
     connect();
-  }, [signer]);
+
+  }, [signer, signerAddress]);
 
   const inserirNota = async (e) => {
     const input = document.getElementsByClassName('card-input')[0]
     const data = input.value
     const [idAluno, idDisciplina, nota] = data.split(',')
     if(idAluno != "" && idDisciplina != "" && nota != ""){
-      const resultInserirNota = await contract.inserirNota(parseInt(idAluno), parseInt(idDisciplina), parseInt(nota));
+      const resultInserirNota = await professorContract.inserirNota(parseInt(idAluno), parseInt(idDisciplina), parseInt(nota));
       resultInserirNota.wait(1);
       console.log(resultInserirNota);
+    }
+    else{
+      alert("Please provide a non-blank/valid input information.")
     }
   }
 
@@ -47,10 +52,11 @@ const Professor = () => {
     const data = input.value
     const idDisciplina = data
     if(idDisciplina != ""){
-      
-      const resultListarNotas = await contract.listarNotasDisciplina(parseInt(idDisciplina));
-      resultListarNotas.wait(1);
+      const resultListarNotas = await professorContract.listarNotasDisciplina(parseInt(idDisciplina));
       console.log(resultListarNotas);
+    }
+    else{
+      alert("Please provide a non-blank/valid input information.")
     }
   }
     return (
